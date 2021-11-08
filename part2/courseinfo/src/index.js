9764b832e9ac6aaa2ff6d60c428b44bfc6283486
dianyehezkiel <dianyehezkiel@gmail.com>
2021-11-08 07:49:45 +0700

    done exercise 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,44 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Course from './components/Course';
 
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return(
-    <p><b>Total of {sum} exercises</b></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
 
 const App = () => {
   const courses = [
