c7d9c9d97769f6c8b12f896f058fb8e3c746cc82
dianyehezkiel <dianyehezkiel@gmail.com>
2021-11-10 21:57:37 +0700

    done exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
@@ -7,13 +10,17 @@ const App = () => {
     { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
     { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' },
   ]);
-  const [ newName, setNewName ] = useState('');
-  const [ newNumber, setNewNumber ] = useState('');
-  const [ filter, setFilter ] = useState('');
+  const [ filteredPersons, setFilteredPersons ] = useState(null);
 
-  const addPerson = (event) => {
-    event.preventDefault();
+  const filterChange = (filter) => {
+    const fp = filter
+      ? persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+      : null;
+        
+    setFilteredPersons(fp)
+  };
 
+  const addPerson = (newName, newNumber) => {
     const personExist = persons.find((person) => {
       return person.name.toLowerCase() === newName.toLowerCase();
     });
@@ -29,58 +36,19 @@ const App = () => {
       };
 
       setPersons(persons.concat(newPerson));
-      setNewName('');
-      setNewNumber('');
+
+      return 1;
     }
   };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const filteredPersons = filter
-    ? persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
-    : persons;
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input 
-        placeholder="Name..."
-        value={filter}
-        onChange={handleFilterChange} />
-      </div>
-      <h2>Add a New Contact</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input 
-            placeholder="Add Name"
-            value={newName}
-            onChange={handleNameChange} />
-        </div>
-        <div>number: <input 
-          placeholder="Add Phonenumber"
-          value={newNumber}
-          onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-
-      <h2>Numbers</h2>
-      {filteredPersons.map(person => 
-        <p key={person.id}>{person.name} {person.number}</p>)
-      }
+      <Filter onChange={filterChange} />
+      <h3>Add a New Contact</h3>
+      <PersonForm onSubmit={addPerson} />
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons? filteredPersons : persons} />
     </div>
   );
 };
