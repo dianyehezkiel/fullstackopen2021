e657b8c7bde2e21cdff3e7ab22b2864ec8d30d34
dianyehezkiel <dianyehezkiel@gmail.com>
2021-11-13 20:57:49 +0700

    done exercise 2.14

--- part2/datacountries/src/components/Country.js
+++ part2/datacountries/src/components/Country.js
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Weather from "./Weather";
+
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`;
+
+    axios
+      .get(url)
+      .then(response => {
+        setWeather(response.data);
+      })
+  }, [country.capital]);
+
   return (
     <div>
       <h2>{country.name}</h2>
@@ -11,7 +27,8 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={country.flag} alt={country.name} width="150" />
-    </div>
+      <Weather weather={weather} />
+    </div>  
   );
 }
 
